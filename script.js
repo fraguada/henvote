abddb20d0c527206624edd155ae162b6c01638a5
Luis Fraguada <fraguada@gmail.com>
2021-04-27 23:30:36 +0200

    basic test with octokit

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-
+import { Octokit } from 'https://cdn.skypack.dev/@octokit/core'
 console.log( 'hi' )
+
+const octokit = new Octokit()
+
+getit()
+
+async function getit() {
+
+    const response = await octokit.request("GET /orgs/{org}/repos", {
+        org: "hicetnunc2000",
+        type: "public",
+    })
+
+    console.log( response )
+}
